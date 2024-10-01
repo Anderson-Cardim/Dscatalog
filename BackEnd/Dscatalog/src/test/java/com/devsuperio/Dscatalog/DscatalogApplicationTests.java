3745ee0f6b47eb9b20f4168a653f7a0008d0dbac
Anderson Cardim <cardimanderson0@gmail.com>
2024-10-01 14:09:56 -0300

    Backend project created

---
+++ BackEnd/Dscatalog/src/test/java/com/devsuperio/Dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperio.Dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
